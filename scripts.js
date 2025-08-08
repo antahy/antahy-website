cab68142f165c94014770a4c1da348dffd316a67
ant <82479346+antahy@users.noreply.github.com>
2025-08-08 16:13:27 -0400

    finished v1 new
    finished v1 new

--- scripts.js
+++ scripts.js
@@ -4,30 +4,6 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-const githubBtn = document.getElementById("githubBtn")
-const blueskyBtn = document.getElementById("blueskyBtn")
-const steamBtn = document.getElementById("steamBtn")
 
-
-githubBtn.addEventListener("click", function(e) {
-
-    console.log("github button was clicked");
-    window.location.href = "https://github.com/antahy";
-
-})
-
-blueskyBtn.addEventListener("click", function(e) {
-
-    console.log("bluesky button was clicked");
-    window.location.href = "https://bsky.app/profile/antahy.github.io";
-
-})
-
-steamBtn.addEventListener("click", function(e) {
-
-    console.log("steam button was clicked");
-    window.location.href = "https://steamcommunity.com/id/antahy/";
-
-})
 
 
