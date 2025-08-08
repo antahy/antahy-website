35b5115a1aace48a392a236726a1f3641c0ac237
ant <82479346+antahy@users.noreply.github.com>
2025-08-07 21:21:45 -0400

    new
    started new

--- scripts.js
+++ scripts.js
@@ -1,4 +1,33 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("the website loaded yaaay");
+    console.log("website loaded");
 });
+
+
+const githubBtn = document.getElementById("githubBtn")
+const blueskyBtn = document.getElementById("blueskyBtn")
+const steamBtn = document.getElementById("steamBtn")
+
+
+githubBtn.addEventListener("click", function(e) {
+
+    console.log("github button was clicked");
+    window.location.href = "https://github.com/antahy";
+
+})
+
+blueskyBtn.addEventListener("click", function(e) {
+
+    console.log("bluesky button was clicked");
+    window.location.href = "https://bsky.app/profile/antahy.github.io";
+
+})
+
+steamBtn.addEventListener("click", function(e) {
+
+    console.log("steam button was clicked");
+    window.location.href = "https://steamcommunity.com/id/antahy/";
+
+})
+
+
